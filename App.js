3c21dd677f1f939d8d05a0c157859687b95da776
Agya Rahmani <agyard7@gmail.com>
2018-10-12 13:13:42 +0700

    image

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -25,6 +25,9 @@ export default class App extends Component<Props> {
         <Text style={styles.instructions}>Nama : Agya Rahmani Divasasri</Text>
         <Text style={styles.instructions}>Kelas : XI RPL 1</Text>
         <Text style={styles.instructions}>No. Absen : 02</Text>
+        <Image
+          style={{width: 200, height: 200}}
+          source={require('./me.jpg')}/>
       </View>
     );
   }
